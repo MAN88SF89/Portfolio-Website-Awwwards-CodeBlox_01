3aa9dbe7d556ca2bbc9a524d959711dba4f4f5ce
Jhulan Dey <jhulandey@gmail.com>
2025-08-12 20:08:45 +0530

    started am awward winning portfolio website re-creation

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll(".reveal")
+.forEach(function (elem) {
+    let spanParent = document.createElement('span');
+    let spanChild = document.createElement('span');
+
+    spanParent.classList.add('parent');
+    spanChild.classList.add('child');
+
+    spanChild.textContent = elem.textContent;
+    spanParent.appendChild(spanChild);
+
+    elem.innerHTML = "";
+    elem.appendChild(spanParent);
+
+})
